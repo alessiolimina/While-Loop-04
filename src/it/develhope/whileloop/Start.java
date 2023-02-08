d2aece2d82086940037b5596bd2c7d07884194af
Alessio Limina <alessio.limina@gmail.com>
2023-02-08 17:21:43 +0100

    Exercise While Loop 04 with Javadoc

--- src/it/develhope/whileloop/Start.java
+++ src/it/develhope/whileloop/Start.java
@@ -3,17 +3,37 @@ package it.develhope.whileloop;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * This class is a Tester Class
+ * @author alessiolimina
+ */
+
 public class Start {
     public static void main(String[] args) {
 
         System.out.println("--------------Starting--------------");
+
+        /**
+         * Declaring the variables
+         */
 
         int number;
         Scanner input = new Scanner(System.in);
         Random randomNumber = new Random();
         int userNumber;
 
+        /**
+         * Starting the program
+         */
+
         System.out.println("Welcome! Try to guess the number! Type a number: ");
+
+        /**
+         * Generating a random number
+         * Typing a user number
+         * Doing a Do/While Loop until the user number is equal to the random number
+         * Ending the program
+         */
 
         number = randomNumber.nextInt(1, 10);
         do {userNumber = input.nextInt();
